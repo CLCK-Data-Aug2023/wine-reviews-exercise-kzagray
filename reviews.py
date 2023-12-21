dcd26ceb02f0504c8fce46f65e76c0072a601db9
Kayla Zagray <kaylaizagray@gmail.com>
2023-12-21 12:44:01 -0500

    re push after being in venv

--- reviews.py
+++ reviews.py
@@ -6,5 +6,6 @@ reviews = pd.read_csv("../content/winemag-data-130k-v2.csv.zip", index_col=0)
 #group by number of reviews per country and average of point reviews per country into dataframe
 summary = pd.DataFrame({"count": reviews.groupby("country"["points"].count(), 
                         "points": reviews.groupby("country").points.mean().round(1)})
+
 #write summary to csv file
 summary.to_csv("reviews-per-country.csv")
