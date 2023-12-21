a613b03c69871f74ccfbc608fca4e9f730d9a3f4
Kayla Zagray <kaylaizagray@gmail.com>
2023-12-21 13:05:37 -0500

    changed index code

--- reviews.py
+++ reviews.py
@@ -1,11 +1,8 @@
 import pandas as pd
 
-#read the csv file
-reviews = pd.read_csv("../content/winemag-data-130k-v2.csv.zip", index_col=0)
+reviews = pd.read_csv("../content/winemag-data-130k-v2.csv.zip")
 
-#group by number of reviews per country and average of point reviews per country into dataframe
-summary = pd.DataFrame({"count": reviews.groupby("country"["points"].count(), 
+summary = pd.DataFrame({"count": reviews.groupby("country")["points"].count(), 
                         "points": reviews.groupby("country").points.mean().round(1)})
 
-#write summary to csv file
-summary.to_csv("reviews-per-country.csv")
+summary.to_csv("reviews-per-country.csv", index = True)
